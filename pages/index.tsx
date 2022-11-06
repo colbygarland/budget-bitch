7ba6841f7217be0881299da86286f3fff3e5aac6
Colby Garland <colbygarland@gmail.com>
2022-11-05 19:40:41 -0600

    add styled components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,18 @@
 import { SITE_TITLE } from '../strings';
 import { Container, Heading, Button } from '@chakra-ui/react';
 import { useUser } from '../services/auth/useUser';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: green;
+`;
 
 export default function Home() {
   const { user, logout } = useUser();
   return (
     <Container>
       <Heading as="h1">{SITE_TITLE}</Heading>
-      <p>Welcome, {user?.displayName}</p>
+      <Title>Welcome, {user?.displayName}</Title>
       <Button onClick={logout}>Logout</Button>
     </Container>
   );
