b95df0fb02e8faaf2062acc5403284adf213227b
Colby Garland <colbygarland@gmail.com>
2022-11-07 21:17:51 -0700

    refactor

---
+++ components/forms/AddExpenseForm.tsx
@@ -0,0 +1,111 @@
+import { FormControl, FormLabel, Select, FormErrorMessage, Input } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { addExpense, getExpenseTypes } from '../../services/api/expense';
+import { Button } from '../Button';
+
+const FormBlock = styled(FormControl)`
+  margin-bottom: 10px;
+`;
+
+export const AddExpenseForm = ({ onClose }: { onClose: () => void }) => {
+  const [expenseTypes, setExpenseTypes] = useState<string[]>([]);
+  const [selectedExpenseType, setSelectedExpenseType] = useState<string | null>(null);
+  const [selectedExpenseError, setSelectedExpenseError] = useState('');
+  const [newExpenseType, setNewExpenseType] = useState('');
+  const [amount, setAmount] = useState<number | null>(null);
+  const [amountError, setAmountError] = useState('');
+
+  function resetState() {
+    setSelectedExpenseType(null);
+    setNewExpenseType('');
+    setAmount(null);
+    setSelectedExpenseError('');
+    setAmountError('');
+  }
+
+  async function getTypes() {
+    getExpenseTypes().then((types) => {
+      if (types && types.length) {
+        setExpenseTypes(types);
+      }
+    });
+  }
+
+  async function handleOnClick() {
+    if (selectedExpenseType === null) {
+      setSelectedExpenseError('Please select an Expense Type.');
+    }
+    if (amount === null) {
+      setAmountError('Please enter a valid amount.');
+      return;
+    }
+    // if adding a new expense type, use that instead
+    addExpense(newExpenseType !== '' ? newExpenseType : (selectedExpenseType as unknown as string), amount);
+    await getTypes();
+    onClose();
+    resetState();
+  }
+
+  function handleClose() {
+    onClose();
+    resetState();
+  }
+
+  useEffect(() => {
+    getTypes();
+  }, []);
+
+  return (
+    <>
+      <FormBlock isInvalid={selectedExpenseError}>
+        <FormLabel>Expense Type</FormLabel>
+        <Select
+          placeholder="Select Expense Type"
+          value={selectedExpenseType as unknown as string}
+          onChange={(e) => {
+            setSelectedExpenseType(e.target.value);
+            setSelectedExpenseError('');
+          }}
+        >
+          {expenseTypes?.map((type) => (
+            <option key={type} value={type}>
+              {type.charAt(0).toUpperCase() + type.slice(1)}
+            </option>
+          ))}
+          <option value="add-new">+ Add New Type</option>
+        </Select>
+        {selectedExpenseError && <FormErrorMessage>{selectedExpenseError}</FormErrorMessage>}
+      </FormBlock>
+      {selectedExpenseType === 'add-new' && (
+        <FormBlock>
+          <FormLabel>New Expense Type</FormLabel>
+          <Input
+            placeholder="Expense type"
+            type="text"
+            onChange={(e) => {
+              setNewExpenseType(e.target.value);
+            }}
+          />
+        </FormBlock>
+      )}
+      <FormBlock isInvalid={amountError}>
+        <FormLabel>Amount</FormLabel>
+        <Input
+          placeholder="10"
+          type="number"
+          value={amount as number}
+          onChange={(e) => {
+            setAmount(e.target.value as unknown as number);
+            setAmountError('');
+          }}
+        />
+        {amountError && <FormErrorMessage>{amountError}</FormErrorMessage>}
+      </FormBlock>
+      <Button onClick={handleOnClick}>+ Add Expense</Button>
+      <Button onClick={handleClose} type="outline">
+        Close
+      </Button>
+    </>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,24 +4,19 @@ import { Loader } from '../components/Loader';
 import { Header } from '../components/Header';
 import { colors } from '../theme/colors';
 import { Button } from '../components/Button';
-import { useEffect, useState } from 'react';
-import { addExpense, Expense, getExpenseTypes, useGetExpenses } from '../services/api/expense';
+import { Expense, useGetExpenses } from '../services/api/expense';
 import {
   useDisclosure,
   Modal,
   ModalOverlay,
   ModalContent,
   ModalHeader,
-  ModalFooter,
   ModalBody,
   ModalCloseButton,
   Container,
-  Input,
-  Select,
-  FormControl,
-  FormLabel,
 } from '@chakra-ui/react';
 import { zIndex } from '../theme/zIndex';
+import { AddExpenseForm } from '../components/forms/AddExpenseForm';
 
 const Page = styled.div`
   background-color: ${colors.primary};
@@ -63,10 +58,6 @@ const FloatingButton = styled(Button)`
   z-index: ${zIndex[100]};
 `;
 
-const FormBlock = styled(FormControl)`
-  margin-bottom: 10px;
-`;
-
 function calculateExpenses(expenses: Expense[]) {
   let total = 0;
   expenses.map((expense) => {
@@ -78,46 +69,8 @@ function calculateExpenses(expenses: Expense[]) {
 export default function Home() {
   const { user } = useUser();
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [expenseTypes, setExpenseTypes] = useState<string[]>([]);
-  const [selectedExpenseType, setSelectedExpenseType] = useState<string | null>(null);
-  const [newExpenseType, setNewExpenseType] = useState('');
-  const [amount, setAmount] = useState<number | null>(null);
+
   const expenses = useGetExpenses();
-
-  function resetState() {
-    // reset the state
-    setSelectedExpenseType(null);
-    setNewExpenseType('');
-    setAmount(null);
-  }
-
-  async function handleOnClick() {
-    if (selectedExpenseType === null || amount === null) {
-      return; // TODO: add error handling
-    }
-    // if adding a new expense type, use that instead
-    addExpense(newExpenseType !== '' ? newExpenseType : (selectedExpenseType as unknown as string), amount);
-    await getTypes();
-    onClose();
-    resetState();
-  }
-
-  function handleClose() {
-    onClose();
-    resetState();
-  }
-
-  async function getTypes() {
-    getExpenseTypes().then((types) => {
-      if (types && types.length) {
-        setExpenseTypes(types);
-      }
-    });
-  }
-
-  useEffect(() => {
-    getTypes();
-  }, []);
 
   if (!user) {
     return (
@@ -135,57 +88,15 @@ export default function Home() {
       </Price>
       <p>Out of $4,000 budgeted for this month</p>
       <FloatingButton onClick={onOpen}>+ Add Expense</FloatingButton>
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={isOpen} onClose={onClose} size="sm">
         <ModalOverlay />
         <Container></Container>
         <ModalContent>
           <ModalHeader>Add Expense</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <FormBlock>
-              <FormLabel>Expense Type</FormLabel>
-              <Select
-                placeholder="Select Expense Type"
-                value={selectedExpenseType as unknown as string}
-                onChange={(e) => setSelectedExpenseType(e.target.value)}
-              >
-                {expenseTypes?.map((type) => (
-                  <option key={type} value={type}>
-                    {type.charAt(0).toUpperCase() + type.slice(1)}
-                  </option>
-                ))}
-                <option value="add-new">+ Add New Type</option>
-              </Select>
-            </FormBlock>
-            {selectedExpenseType === 'add-new' && (
-              <FormBlock>
-                <FormLabel>New Expense Type</FormLabel>
-                <Input
-                  placeholder="Expense type"
-                  type="text"
-                  onChange={(e) => {
-                    setNewExpenseType(e.target.value);
-                  }}
-                />
-              </FormBlock>
-            )}
-            <FormBlock>
-              <FormLabel>Amount</FormLabel>
-              <Input
-                placeholder="10"
-                type="number"
-                value={amount as number}
-                onChange={(e) => setAmount(e.target.value as unknown as number)}
-              />
-            </FormBlock>
+            <AddExpenseForm onClose={onClose} />
           </ModalBody>
-
-          <ModalFooter>
-            <Button onClick={handleOnClick}>+ Add Expense</Button>
-            <Button onClick={handleClose} type="outline">
-              Close
-            </Button>
-          </ModalFooter>
         </ModalContent>
       </Modal>
       {expenses.map((expense) => {
